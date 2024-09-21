e130706440e03e313224b760a264dbb971fa888b
Arun <arunkushwaha89765@gmail.com>
2024-09-21 16:03:22 +0530

    Create class to handle error

---
+++ src/errors/index.ts
@@ -0,0 +1,198 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+
+export function handleError(error: Error): Error {
+  if (error instanceof CustomHttpException) {
+    throw error;
+  }
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    console.error('Prisma Client Known Request Error:', error.message);
+
+    if (error.code === 'P2002') {
+      return new PrismaUniqueCosntraintError();
+    } else if (error.code === 'P2003') {
+      return new PrismaForeignKeyCosntraintError();
+    } else if (error.code === 'P2025') {
+      return new PrismaUpdateError();
+    } else if (error.code === 'P2026') {
+      return new PrismaUniquePropertyError();
+    } else if (error.code === 'P2027') {
+      return new PrismaMissingRecordError();
+    } else if (error.code === 'P2023') {
+      return new PrismaInvalidRecordError();
+    } else {
+      return new PrismaDefaultError();
+    }
+  } else if (error instanceof Prisma.PrismaClientValidationError) {
+    return new PrismaClientValidationError();
+  } else if (error instanceof Prisma.PrismaClientInitializationError) {
+    return new PrismaClientInitializationError();
+  } else if (error instanceof Prisma.PrismaClientRustPanicError) {
+    return new PrismaClientRustPanicError();
+  } else {
+    return new InternalServerError();
+  }
+}
+
+class CustomHttpException extends HttpException {
+  constructor(message: string, status: number) {
+    super(message, status);
+  }
+}
+
+export class PrismaInvalidRecordError extends CustomHttpException {
+  constructor() {
+    super('Invalid record error', HttpStatus.BAD_REQUEST);
+  }
+}
+export class PrismaUniqueCosntraintError extends CustomHttpException {
+  constructor() {
+    super(
+      'A record with the same unique property already exists. Please check your input and try again',
+      HttpStatus.CONFLICT,
+    );
+  }
+}
+export class PrismaForeignKeyCosntraintError extends CustomHttpException {
+  constructor() {
+    super(
+      'The operation failed due to a foreign key constraint. Please check related records',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+
+export class PrismaUpdateError extends CustomHttpException {
+  constructor() {
+    super(
+      'The record you are trying to update does not exist',
+      HttpStatus.NOT_FOUND,
+    );
+  }
+}
+
+export class PrismaUniquePropertyError extends CustomHttpException {
+  constructor() {
+    super(
+      'A required unique property is missing or invalid.',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+
+export class PrismaMissingRecordError extends CustomHttpException {
+  constructor() {
+    super(
+      'The data provided is invalid. Please review your input and try again',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+export class PrismaDefaultError extends CustomHttpException {
+  constructor() {
+    super(
+      'A known error occurred while processing your request. Please try again',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+export class PrismaClientValidationError extends CustomHttpException {
+  constructor() {
+    super(
+      'There was a validation error with your request. Please check the input values',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+export class PrismaClientInitializationError extends CustomHttpException {
+  constructor() {
+    super(
+      'There was an error initializing the database connection. Please contact support if the problem persists',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+export class PrismaClientRustPanicError extends CustomHttpException {
+  constructor() {
+    super(
+      'An unexpected error occurred in the application. Please contact support',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+export class InvalidToken extends CustomHttpException {
+  constructor() {
+    super('Invalid token', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class NoToken extends CustomHttpException {
+  constructor() {
+    super('Unauthorized', HttpStatus.UNAUTHORIZED);
+  }
+}
+
+export class InvalidCredentials extends CustomHttpException {
+  constructor() {
+    super('Invalid Credentials', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class EmailExists extends CustomHttpException {
+  constructor() {
+    super('User already registered, try signing', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class InternalServerError extends CustomHttpException {
+  constructor() {
+    super(
+      'An unexpected error occurred. Please try again later',
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
+export class InvalidUser extends CustomHttpException {
+  constructor() {
+    super(
+      'Invalid! no user exits with provided user id',
+      HttpStatus.BAD_REQUEST,
+    );
+  }
+}
+
+export class DataFetchError extends CustomHttpException {
+  constructor() {
+    super('Unable to your reterive data', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export class JobPostAlreadyExists extends HttpException {
+  constructor() {
+    super('Job Post already exists', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class InvalidPost extends HttpException {
+  constructor() {
+    super('Invalid post', HttpStatus.BAD_REQUEST);
+  }
+}
+
+export class InvalidJSONFromLLM implements Error {
+  name: 'Invalid JSON response';
+  message: 'Invalid JSON object returned from LLM';
+}
+
+export class InvalidResumeFormat implements Error {
+  name: 'Invalid resume format';
+  message: 'Unable to read resume';
+}
+export class PlanAlreadyBought extends HttpException {
+  constructor() {
+    super('User already has a plan', HttpStatus.BAD_REQUEST);
+  }
+}
